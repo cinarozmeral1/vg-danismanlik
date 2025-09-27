06fe0c2333c0c010e24c4c451e28c8a9ddb47d33
Cinar Ozmeral <cinarozmeral1@gmail.com>
2025-09-27 22:43:50 +0200

    Change university detail route to /go/:id to bypass Vercel protection

--- server.js
+++ server.js
@@ -389,7 +389,7 @@ app.use('/user', userRoutes);
 app.use('/admin/guardians', guardianRoutes);
 
 // Public university routes (no authentication required)
-app.get('/look/:id', async (req, res) => {
+app.get('/go/:id', async (req, res) => {
     try {
         const universityId = req.params.id;
 
